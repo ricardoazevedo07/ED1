902fe7f9afc5f3832518cb74cfa2c056a74a5221
Ricardo <rick.azevedo07@gmail.com>
2017-07-17 21:47:32 -0300

    função de criação pronta

--- ArvoreABB/ABB.h
+++ ArvoreABB/ABB.h
@@ -1,6 +1,13 @@
-\\funções de arvore binaria de busca
+//funções de arvore binaria de busca
 
 typedef struct arvore{
 char info
 struct *arvore;
 }ARVORE;
+
+//ABAIXO FUNÇÕES DE CRIAÇÃO DA ARVORE.
+
+ARVORE *CRIA_VAZIA(){
+return NULL;
+}
+
