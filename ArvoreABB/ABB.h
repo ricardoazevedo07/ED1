c1a8618a95ac57154faa032b3e6a250cf772677b
Ricardo <rick.azevedo07@gmail.com>
2017-07-17 21:58:38 -0300

    criada função de inserção

--- ArvoreABB/ABB.h
+++ ArvoreABB/ABB.h
@@ -1,9 +1,9 @@
 //funções de arvore binaria de busca
 
 typedef struct arvore{
-char info
-struct *arvore dir;
-struct *arvore esq;
+int info;
+struct *arvore DIR;
+struct *arvore ESQ;
 
 }ARVORE;
 
@@ -15,5 +15,14 @@ return NULL;
 
 //FUNÇÃO DE INSERÇÃO NA ARVORE.
 
+ARVORE *INSERE(int info, ARVORE *esq, ARVORE *dir){
+ARVORE *NOVA = (ARVORE *) malloc(sizeof(ARVORE));
+
+NOVA->info = info;
+NOVA->ESQ = esq;
+NOVA->DIR = dir;
+
+return NOVA;
+}
 
 
